15d2998b214131f08c4451dd34ef706974e6fa8f
TeamsSix <49087564+teamssix@users.noreply.github.com>
2021-04-21 15:20:11 +0800

    v0.5.2

--- tig.py
+++ tig.py
@@ -23,7 +23,6 @@ from configparser import ConfigParser
 console = Console()
 requests.packages.urllib3.disable_warnings()
 
-
 def random_useragent():
     ua = [
         "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
